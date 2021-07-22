8c50730cc62b94a29e2609d67c12f7a675524807
Foland <Daniel.Foland@Daniel-MacPro.home>
2021-07-22 11:36:47 -0700

    added test file with unittest framework

---
+++ neo4j_db_api_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import unittest
+import neobolt
+import warnings
+from db_docker import Neo4jDockerDB
+from neo4j_db_api import Neo4jAPIException
+from neobolt import exceptions
+
+# ignore warnings during import time or it won't work
+warnings.filterwarnings("ignore", message="can't resolve package \
+    from __spec__ or __package__")
+warnings.filterwarnings("ignore", message="ResourceWarning: unclosed")
+
+class TestDatabase(unittest.TestCase):
+    # test functions exectuted in alphabetical order
+    def test_1_do_stuff(self):
+        # create and start neo4j container
+        neo4j_docker_db = Neo4jDockerDB()
+        neo4j_docker_db.start()
