67d9f19396622d526258c3aadf60fcfd2baa89d8
Foland <Daniel.Foland@Daniel-MacPro.home>
2021-07-21 23:37:21 -0700

    updated API and created object port mapping

--- db_docker.py
+++ db_docker.py
@@ -1,5 +1,7 @@
 import docker
 from neo4j_db_api import Neo4jDB
+import os
+
 
 class Neo4jDockerDB(Neo4jDB):
     #TODO child class for launch docker DB
@@ -7,12 +9,18 @@ class Neo4jDockerDB(Neo4jDB):
 
     def start(self):
         #TODO start docker container
-        # run with kwargs
-        pass
+        # run with bash command
+        cmd = "docker run --name neo4jdb " \
+              "-p7474:7474 -p7687:7687 -d " \
+              "--env NEO4J_AUTH=neo4j/test neo4j:3.5.17"
+        os.system(cmd)
+
+
 
     def stop(self):
-        #TODO docker stop container
-        pass
+        try:
+            self.container.stop()
+
 
     def __del__(self):
         #TODO handle kill to stop container
--- neo4j_db_api.py
+++ neo4j_db_api.py
@@ -1,39 +1,100 @@
-from neo4j import GraphDatabase, basic_auth
+from neo4j import GraphDatabase, basic_auth, CypherError
+import subprocess
+import os
+
+
+class Neo4jAPIException(Exception):
+    '''
+    Generic Exception class for the Neo4jDB API class
+    '''
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return 'Neo4jAPIException, {0} '.format(self.message)
+        else:
+            return 'Neo4jAPIException has been raised.'
 
 
 class Neo4jDB:
-    def __init__(self, kwargs={}):
-        #TODO set up ports and driver
-
+    '''
+    This is the api class that contains the connector, db port info,
+    login credentials, and query functions to pull information from
+    the neo4j container (set up in the db_docker child class)
+    '''
+    def __init__(self, ip="127.0.0.1", bolt=7687, http=7475):
+        # set to localhost
+        self.ip = ip
+        # set object to have default bolt port
+        self.bolt = bolt
+        # start database driver with default password
+        pw = os.environ.get("NEO4J_PASSWORD") or "test"
+        # set http port
+        self.http = http
+        # create db connector
         self.driver = GraphDatabase.driver(
-            "bolt://%s:%s/" % (self.host, self.bolt_port),
-            auth=basic_auth("neo4j", self.pw))
+            "bolt://%s:%d/" % (self.ip, self.bolt),
+            auth=basic_auth("neo4j", pw))
 
-    def check_response(self):
-        #TODO curl server to check response
-        pass
+    def curl_db(self):
+        '''
+        Check if the neo4j db is responds
+        via the selected port in the container
+        :return: True if curl got an OK response, False otherwise
+        '''
+        result = subprocess.getoutput('curl -s -I  %s:%d | grep "200 OK"'
+                                      % (self.ip, self.http))
+        # '200 OK' in curl result indicates success
+        if "200 OK" in result:
+            return True
+        else:
+            return False
 
     def run_query(self, cmd):
-        # TODO execute command
+        '''
+        Execute a single Cypher query
+        :param cmd: Cypher query string to execute
+        :return: server response from Cypher command
+        '''
         with self.driver.session() as session:
             tx = session.begin_transaction()
             try:
-                session.run(cmd)
+                ret = session.run(cmd)
                 tx.commit()
+                return ret
+            except CypherError as ex:
+                print(f"Cypher command failed: {cmd}")
+                raise Neo4jAPIException("Neo4j run_query failure.")
             except Exception as ex:
-                print(f"Query failed: {cmd}")
                 print("Exception: ", type(ex).__name__, ex.args)
                 # attempt a rollback, if possible
                 try:
                     # database in possibly corrupt state, rollback
                     tx.rollback()
                 except:
+                    # unable to rollback, just continue
                     pass
-
+                raise Neo4jAPIException("Neo4j run_query failure.")
 
     def create_node(self, label, properties):
-        # TODO create new node, return id
-        pass
+        '''
+        Create node with input properties and labels
+        :param label: string of ':' delimited labels
+        :param properties: Cypher compliant properties
+        :return: neo4j ID of the node created
+        '''
+        command = "CREATE (n:%s {%s}) " \
+                  "RETURN ID(n)" % (label, properties)
+        ret = self.run_query(command)
+        try:
+            # return response data
+            return ret.data()[0]['ID(n)']
+        except Exception:
+            raise Neo4jAPIException("Neo4j create_node failure: %s" % command)
 
     def create_relationship_by_id(self, source_id, destination_id, rel):
         # TODO create relationship between two nodes with matching ID's
